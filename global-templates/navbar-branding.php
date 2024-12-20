3fb3ef535b8f184446ef8366d1881239823ccde4
woodwardtw <twwoodward@me.com>
2024-12-20 13:37:34 -0500

    header to png

--- global-templates/navbar-branding.php
+++ global-templates/navbar-branding.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
 
 if ( ! has_custom_logo() ) { ?>
  <div class="main-title">
- 	<a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri();?>/imgs/bpbq-title.svg" class="img-fluid dd-title" alt="Demystifying AI: Big promises, big questions."></a>
+ 	<a href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri();?>/imgs/bpbq-title.png" class="img-fluid dd-title" alt="Demystifying AI: Big promises, big questions."></a>
     <h1 id="mag-title" class="sr-only"><a href="<?php echo get_home_url(); ?>"><?php echo get_bloginfo('name')?></a></h1>
     <div class="sub-title"><?php echo get_bloginfo('description')?></div>
 </div>
